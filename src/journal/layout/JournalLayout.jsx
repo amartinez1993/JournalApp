3ea34a6e6e1e49b3a16e459378163706de8caa60
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-09-26 17:28:13 +0200

    Sidebar created

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components/NavBar";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar} from "../components";
 
 const drawerWidth = 240;
 
@@ -9,14 +9,14 @@ export const JournalLayout = ({ children }) => {
 
         <NavBar drawerWidth={ drawerWidth }/>
 
-        {/* Sidebar drawerWidth*/}
+        <SideBar drawerWidth={ drawerWidth }/>
 
         <Box 
             component='main'
             sx={{ flexGrow: 1, p: 3 }}
         >
 
-            {/* Toolbar */}
+            <Toolbar />
 
             { children }
 
