dd6a90e63c3cb88fd6b8ad0c145ae687f2d34c33
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-09-26 16:59:29 +0200

    Navbar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -4,7 +4,13 @@ import { JournalLayout } from "../layout/JournalLayout"
 export const JournalPage = () => {
   return (
     <JournalLayout>
+
       <Typography>Mollit consectetur irure non ad velit pariatur sit esse eiusmod culpa proident. Laboris elit ex quis aliqua qui enim voluptate laboris et cupidatat officia. Lorem nostrud enim anim do nostrud minim sit fugiat excepteur incididunt adipisicing.</Typography>
+    
+      {/* NothingSelected */}
+
+      {/* NoteView */}
+    
     </JournalLayout>
   )
 }
