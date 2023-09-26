cbd031b003d82dc3cb749e50a3b42f0347cda857
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-09-26 16:49:38 +0200

    JournalLayout and JournalPage created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Mollit consectetur irure non ad velit pariatur sit esse eiusmod culpa proident. Laboris elit ex quis aliqua qui enim voluptate laboris et cupidatat officia. Lorem nostrud enim anim do nostrud minim sit fugiat excepteur incididunt adipisicing.</Typography>
+    </JournalLayout>
   )
 }
