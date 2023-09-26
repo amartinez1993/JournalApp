52692bc8501865b09323754f9f36045d02aa39f7
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-09-26 17:46:26 +0200

    added views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,16 @@
-import { Typography } from "@mui/material"
+
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView, NoteView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Mollit consectetur irure non ad velit pariatur sit esse eiusmod culpa proident. Laboris elit ex quis aliqua qui enim voluptate laboris et cupidatat officia. Lorem nostrud enim anim do nostrud minim sit fugiat excepteur incididunt adipisicing.</Typography>
-    
-      {/* NothingSelected */}
+      {/* <Typography>Mollit consectetur irure non ad velit pariatur sit esse eiusmod culpa proident. Laboris elit ex quis aliqua qui enim voluptate laboris et cupidatat officia. Lorem nostrud enim anim do nostrud minim sit fugiat excepteur incididunt adipisicing.</Typography> */}
 
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+
+      <NoteView/>
     
     </JournalLayout>
   )
