20471dafcb0435126f2acaf9dadea09ce1eeadeb
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-10-11 11:11:10 +0200

    form validations

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,14 +1,40 @@
 import { Link as RouterLink} from 'react-router-dom';
-import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { useState } from 'react';
 
+const formData = {
+  email: '',
+  password: '',
+  displayName: ''
+}
+
+const formValidations = {
+  email: [ (value) => value.includes('@'), 'Email must have an @'],
+  password: [ (value) => value.length >= 6 , 'Password must be more than 6 characters'],
+  displayName: [ (value) => value.length >= 1 , 'Name is required'],
+}
 
 export const RegisterPage = () => {
+
+  const [formSubmitter, setFormSubmitter] = useState(false);
+
+  const { 
+    formState, displayName, email, password, onInputChange,
+    isFormValid, displayNameValid, emailValid, passwordValid,
+   } = useForm(formData, formValidations);
+
+
+  const onSubmit = (event) => {
+      event.preventDefault();
+      setFormSubmitter(true);
+  }
+
   return (
     <AuthLayout title="Register">
-
-      <form>
+      <h1>FormValid: { isFormValid ? 'valido' : 'incorrecto'} </h1>
+      <form onSubmit={ onSubmit }>
         <Grid container>
 
             <Grid item xs={ 12 } sx={{ mt: 2}}>
@@ -17,6 +43,11 @@ export const RegisterPage = () => {
                 type="text" 
                 placeholder="Fake Name" 
                 fullWidth
+                name="displayName"
+                value={ displayName }
+                onChange={ onInputChange }
+                error={ !!displayNameValid && formSubmitter }
+                helperText={ displayNameValid }
               />
             </Grid>
 
@@ -26,6 +57,11 @@ export const RegisterPage = () => {
                 type="email" 
                 placeholder="mail@google.com" 
                 fullWidth
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+                error={ !!emailValid && formSubmitter }
+                helperText={ emailValid }
               />
             </Grid>
 
@@ -35,12 +71,21 @@ export const RegisterPage = () => {
                 type="password" 
                 placeholder="Password" 
                 fullWidth
+                name="password"
+                value={ password }
+                onChange={ onInputChange }
+                error={ !!passwordValid && formSubmitter }
+                helperText={ passwordValid }
               />
             </Grid>
 
             <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={ 12 }>
-                <Button variant='contained' fullWidth>
+                <Button
+                  type="submit" 
+                  variant='contained' 
+                  fullWidth
+                >
                   Register
                 </Button>
               </Grid>
