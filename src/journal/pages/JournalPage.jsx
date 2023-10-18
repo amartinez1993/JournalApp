935e5cb7b48057d94da94a75919c94b59872df9c
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-10-18 13:48:52 +0200

    Animations Added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -8,11 +8,8 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      {/* <Typography>Mollit consectetur irure non ad velit pariatur sit esse eiusmod culpa proident. Laboris elit ex quis aliqua qui enim voluptate laboris et cupidatat officia. Lorem nostrud enim anim do nostrud minim sit fugiat excepteur incididunt adipisicing.</Typography> */}
-
       <NothingSelectedView />
 
-      {/* <NoteView/> */}
 
       <IconButton
         size='large'
